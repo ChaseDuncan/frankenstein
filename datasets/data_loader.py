7b8e244efea0b2d4fb0d9936a4197d63385fc7fd
Chase Duncan <cddunca2@koyejo-2.cs.illinois.edu>
2019-06-11 20:43:55 -0500

    Adding datasets dir.

---
+++ datasets/data_loader.py
@@ -0,0 +1,31 @@
+import os
+
+import nibabel as nib
+from torch.utils.data import Dataset
+
+class BraTSDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        # store filenames. expects data_dir/{HGG, LGG}/
+        # TODO: should HGG and LGG be separated?
+        self.filenames = \
+                [ data_dir + "/HGG/" + f for f in os.listdir(data_dir + "/HGG/") ]
+        self.filenames.extend([ data_dir + "/LGG/" + f \
+                for f in os.listdir(data_dir + "/LGG/") ])
+        self.filenames = [ f + d for f in self.filenames for d in os.listdir(f) ]
+        # only take t1 files
+        self.input = sorted([ f for f in self.filenames if "t1.nii.gz" in f ])
+        self.segs = sorted([ f for f in self.filenames if "seg.nii.gz" in f ])
+        self.transform = transform
+
+    def __len__(self):
+        # return size of dataset
+        return len(self.input)
+
+    def __getitem__(self, idx):
+        # open image and apply transform if applicable
+        img = nib.load(self.input[idx]).get_fdata()
+        if self.transform not None:
+            img = self.transform(img)
+        seg = nib.load(self.segs[idx]).get_fdata()
+        return img, seg
+
