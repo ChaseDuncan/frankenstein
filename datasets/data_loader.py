Fixed a bunch of problems with the normalization.

datasets/data_loader.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import nibabel as nib
 import torch
-from torchvision import transforms
+import torchvision.transforms.functional as TF
 from torch.utils.data import Dataset
 
 class BraTSDataset(Dataset):
@@ -38,27 +38,34 @@ class BraTSDataset(Dataset):
             t1 = t1[56:-56, 56:-56, 14:-13]
             t1 = torch.from_numpy(t1)
             t1_mean = torch.mean(t1)
+            means = [t1_mean]*t1.shape[0]
             t1_std = torch.std(t1)
-            t1_trans = transforms.Normalize(t1, t1_mean, t1_std)
-            data.append(t1)
+            stds = [t1_std]*t1.shape[0]
+            t1_trans = TF.normalize(t1, means, stds)
+
+            data.append(t1_trans)
 
         if 't1ce' in self.modes:
             t1ce = self.transform(nib.load(self.t1ce[idx]).get_fdata())
             t1ce = t1ce[56:-56, 56:-56, 14:-13]
             t1ce = torch.from_numpy(t1ce)
             t1ce_mean = torch.mean(t1ce)
+            means = [t1ce_mean]*t1ce.shape[0]
             t1ce_std = torch.std(t1ce)
-            t1ce_trans = transforms.Normalize(t1ce, t1ce_mean, t1ce_std)
+            stds = [t1ce_std]*t1ce.shape[0]
+            t1ce_trans = TF.normalize(t1ce, means, stds)
 
-            data.append(t1ce)
+            data.append(t1ce_trans)
 
         if 't2' in self.modes:
             t2 = self.transform(nib.load(self.t2[idx]).get_fdata())
             t2 = t2[56:-56, 56:-56, 14:-13]
             t2 = torch.from_numpy(t2)
             t2_mean = torch.mean(t2)
+            means = [t2_mean]*t2.shape[0]
             t2_std = torch.std(t2)
-            t2_trans = transforms.Normalize(t2, t2_mean, t2_std)
+            stds = [t2_std]*t2.shape[0]
+            t2_trans = TF.normalize(t2, means, stds)
 
             data.append(t2_trans)
 
@@ -67,8 +74,10 @@ class BraTSDataset(Dataset):
             flair = flair[56:-56, 56:-56, 14:-13]
             flair = torch.from_numpy(flair)
             flair_mean = torch.mean(flair)
+            means = [flair_mean]*flair.shape[0]
             flair_std = torch.std(flair)
-            flair_trans = transforms.Normalize(flair, flair_mean, flair_std)
+            stds = [flair_std]*flair.shape[0]
+            flair_trans = TF.normalize(flair, means, stds)
 
             data.append(flair_trans)
 

train.py
@@ -3,8 +3,6 @@ import torch.optim as optim
 import torch.utils.data.sampler as sampler
 import numpy as np
 
-from configparser import ConfigParser
-
 import pickle
 import json
 import argparse
@@ -12,7 +10,7 @@ import argparse
 from tqdm import tqdm
 from utils import (
         dice_score, 
-        parse_config
+        MRISegConfigParser
         )
 from torch.utils.data import DataLoader
 from losses.dice import DiceLoss
@@ -23,25 +21,26 @@ parser = argparse.ArgumentParser(description='Train MRI segmentation model.')
 parser.add_argument('--config')
 args = parser.parse_args()
 
-config = parse_confit(args.config)
+config = MRISegConfigParser(args.config)
+modes = json.loads(config.modes)
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
-brats_data = BraTSDataset(data_dir, modes=modes)
+brats_data = BraTSDataset(config.data_dir, modes=modes)
 num_examples = len(brats_data)
 data_indices = np.arange(num_examples)
 # TODO: Make checkpoints dir if doesn't exist
 
 # Fix stochasticity in data sampling
-if deterministic_train:
+if config.deterministic_train:
     np.random.seed(0)
 
 # TODO: Doesn't really seem to belong here. Make a new
 # class for handling this or push it to the dataloader?
 np.random.shuffle(data_indices)
 
-split_idx = int(num_examples*train_split)
+split_idx = int(num_examples*config.train_split)
 train_sampler = sampler.SubsetRandomSampler(data_indices[:split_idx])
 test_sampler = sampler.SubsetRandomSampler(data_indices[split_idx:])
 trainloader = DataLoader(brats_data, 
@@ -50,7 +49,7 @@ testloader = DataLoader(brats_data,
         batch_size=1, sampler=test_sampler)
 
 # Fix stochasticity in model params, etc.
-if deterministic_train:
+if config.deterministic_train:
     torch.manual_seed(0)
 
 input_channels = len(modes)
@@ -64,7 +63,7 @@ model = model.to(device)
 loss = DiceLoss()
 
 #optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.1)
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=config.weight_decay)
 #optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.1)
 best_loss = 1.0
 
@@ -120,7 +119,7 @@ while(True):
         'eval': avg_eval_dice_by_class,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict()},
-        'checkpoints/'+ model_name)
+        'checkpoints/'+ config.model_name)
 
     avg_eval_dice = torch.sum(avg_eval_dice_by_class) / 3
     if avg_eval_dice > best_eval:
@@ -129,7 +128,7 @@ while(True):
             'eval': avg_eval_dice,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict()},
-            'checkpoints/'+ 'best_' +model_name)
+            'checkpoints/'+ 'best_' + config.model_name)
                 
     best_eval = avg_eval_dice
 

utils.py
@@ -1,24 +1,24 @@
 import torch
-
+from configparser import ConfigParser
 def dice_score(preds, targets):
     num_vec = 2*torch.einsum('cijk, cijk ->c', \
             [preds.squeeze(), targets.squeeze()])
     denom = torch.einsum('cijk, cijk -> c', \
             [preds.squeeze(), preds.squeeze()]) +\
-                torch.einsum('cijk, cijk -> c', \
-                [targets.squeeze(), targets.squeeze()])
+            torch.einsum('cijk, cijk -> c', \
+            [targets.squeeze(), targets.squeeze()])
     dice = num_vec / denom
     return dice
 
-def parse_config(config_file):
-    config = ConfigParser()
-    config.read(config_file)
+class MRISegConfigParser():
+    def __init__(self, config_file):
+        config = ConfigParser()
+        config.read(config_file)
 
-    deterministic_train = config.getboolean('train_params', 'deterministic_train')
-    train_split = config.getfloat('train_params', 'train_split')
-    data_dir = config.get('data', 'data_dir')
-    model_name = config.get('meta', 'model_name')
-    modes = json.loads(config.get('data', 'modes'))
-
-    return config
+        self.deterministic_train = config.getboolean('train_params', 'deterministic_train')
+        self.train_split = config.getfloat('train_params', 'train_split')
+        self.weight_decay = config.getfloat('train_params', 'weight_decay')
+        self.data_dir = config.get('data', 'data_dir')
+        self.model_name = config.get('meta', 'model_name')
+        self.modes = config.get('data', 'modes')
 