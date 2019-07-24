848a575addae60563fcff34b5a4d227f2adbd84a
Ubuntu <chase@languagemodel.risd1mf2wjbu1dro4ebcx3432h.bx.internal.cloudapp.net>
2019-07-24 22:36:16 +0000

    5 fold added.

--- train.py
+++ train.py
@@ -4,13 +4,13 @@ import torch.utils.data.sampler as sampler
 import numpy as np
 
 import pickle
-import json
 import argparse
 
 from tqdm import tqdm
 from utils import (
         dice_score, 
-        MRISegConfigParser
+        MRISegConfigParser,
+        save_model
         )
 from torch.utils.data import DataLoader
 from losses.dice import DiceLoss
@@ -22,118 +22,109 @@ parser.add_argument('--config')
 args = parser.parse_args()
 
 config = MRISegConfigParser(args.config)
-modes = json.loads(config.modes)
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
-brats_data = BraTSDataset(config.data_dir, modes=modes)
-num_examples = len(brats_data)
-data_indices = np.arange(num_examples)
+brats_data = BraTSDataset(config.data_dir, modes=config.modes)
 # TODO: Make checkpoints dir if doesn't exist
 
-# Fix stochasticity in data sampling
-if config.deterministic_train:
-    np.random.seed(0)
+def CV(dataset, batch_size=1, k = 5, deterministic_train=False):
+    num_examples = len(brats_data)
+    data_indices = np.arange(num_examples)
+    if config.deterministic_train:
+        np.random.seed(0)
+    np.random.shuffle(data_indices)
+    folds = np.array(np.split(data_indices, k))
 
-# TODO: Doesn't really seem to belong here. Make a new
-# class for handling this or push it to the dataloader?
-np.random.shuffle(data_indices)
+    cv_trainloader = []
+    cv_testloader = []
 
-split_idx = int(num_examples*config.train_split)
-train_sampler = sampler.SubsetRandomSampler(data_indices[:split_idx])
-test_sampler = sampler.SubsetRandomSampler(data_indices[split_idx:])
-trainloader = DataLoader(brats_data, 
-        batch_size=1, sampler=train_sampler)
-testloader = DataLoader(brats_data, 
-        batch_size=1, sampler=test_sampler)
+    for i in range(len(folds)):
+        mask = np.zeros(len(folds), dtype=bool)
+        mask[i] = True
+        train_folds = np.hstack(folds[~mask])
+        test_fold = folds[mask][0]
+        cv_trainloader.append(DataLoader(dataset, 
+            batch_size, sampler=sampler.SubsetRandomSampler(train_folds)))
+        cv_testloader.append(DataLoader(dataset, 
+            batch_size, sampler=sampler.SubsetRandomSampler(test_fold)))
+    return cv_trainloader, cv_testloader
+
+def train(model, optimizer, train_data_loader, test_data_loader, max_epoch, 
+        name="default", best_eval=0.0, epoch=0):
+    avg_train_losses = []
+
+    while(True):
+        epoch+=1
+        total_loss = 0.0
+        model.train()
+        
+        for train_ex in tqdm(trainloader):
+            optimizer.zero_grad()
+            src, target = train_ex
+            src = src.to(device, dtype=torch.float)
+            target = target.to(device, dtype=torch.float)
+            output = model(src)
+            cur_loss = loss(output, target)
+            total_loss+=cur_loss
+            cur_loss.backward()
+            optimizer.step()
+
+        avg_train_loss = total_loss / len(trainloader)
+        avg_train_losses.append(avg_train_loss)
+
+        sum_test_dice = 0.0
+
+        with torch.no_grad():
+            model.eval()
+            for test_ex in tqdm(testloader):
+                test_src, test_target = test_ex
+                test_src = test_src.to(device, dtype=torch.float)
+                test_target = test_target.to(device, dtype=torch.float)
+                test_output = model(test_src)
+                sum_test_dice += dice_score(test_output, test_target).cpu()
+
+        eval_dice = sum_test_dice / len(testloader)
+
+        print("Saving model after training epoch {}. Average train loss: {} \
+                            Average eval Dice: {}".format(epoch, avg_train_loss, 
+                                                eval_dice))
+        save_model(name, epoch, avg_train_losses, eval_dice, model, optimizer)
+
+        avg_eval_dice = torch.sum(eval_dice) / 3
+        if avg_eval_dice > best_eval:
+            save_model(name, epoch, avg_train_losses, eval_dice, model, optimizer)
+            best_dice_by_class = eval_dice
+
+        best_eval = avg_eval_dice
+
+        if epoch > max_epoch: # TODO: better stopping criteria. Convergence threshold?
+            break
+
+    print("Training complete.")
+    return best_dice_by_class
+
+cv_trainloaders, cv_testloaders = CV(brats_data, deterministic_train=config.deterministic_train)
 
 # Fix stochasticity in model params, etc.
-if config.deterministic_train:
-    torch.manual_seed(0)
 
-input_channels = len(modes)
-model = BraTSSegmentation(input_channels) 
+for i, (trainloader, testloader) in enumerate(zip(cv_trainloaders, cv_testloaders)):
+    if config.deterministic_train:
+        torch.manual_seed(0)
 
-# TODO: continue training from checkpoint
-#checkpoint = torch.load('checkpoints/test')
-#model.load_state_dict(checkpoint['model_state_dict'])
+    input_channels = len(config.modes)
+    model = BraTSSegmentation(input_channels) 
 
-model = model.to(device)
-loss = DiceLoss()
+    model = model.to(device)
+    loss = DiceLoss()
 
-#optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.1)
-optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=config.weight_decay)
-#optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.1)
-best_loss = 1.0
+    #optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.1)
+    #optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.1)
+    model_name = "{}_fold_{}".format(config.model_name, i)
+    optimizer = \
+            optim.Adam(model.parameters(), lr=1e-4, weight_decay=config.weight_decay)
+    train(model, optimizer, trainloader, testloader, config.max_epochs,
+        name=model_name, best_eval=0.0, epoch=0)
 
-epoch = 0
-best_eval = 0.0
-
-# TODO: restart training from checkpoint.
-#epoch = checkpoint['epoch']
-#best_eval = 0.26033
-
-losses = {} # TODO: checkpoint issue
-#losses = pickle.load(open("losses.pkl", "rb"))
-
-while(True):
-    epoch+=1
-    total_loss = 0.0
-    losses[epoch] = []
-    model.train()
-    
-    for train_ex in tqdm(trainloader):
-        optimizer.zero_grad()
-        src, target = train_ex
-        src = src.to(device, dtype=torch.float)
-        target = target.to(device, dtype=torch.float)
-        output = model(src)
-        cur_loss = loss(output, target)
-        total_loss+=cur_loss
-        losses[epoch].append(cur_loss)
-        cur_loss.backward()
-        optimizer.step()
-
-    avg_train_loss = total_loss / num_examples
-
-    pickle.dump(losses, open("losses.pkl", "wb"))
-    model.eval()
-    sum_test_dice = 0.0
-    with torch.no_grad():
-        for test_ex in tqdm(testloader):
-            test_src, test_target = test_ex
-            test_src = test_src.to(device, dtype=torch.float)
-            test_target = test_target.to(device, dtype=torch.float)
-            test_output = model(test_src)
-            sum_test_dice += dice_score(test_output, test_target).cpu()
-
-    avg_eval_dice_by_class = sum_test_dice / len(testloader)
-
-    print("Saving model after training epoch {}. Average train loss: {} \
-                        Average eval Dice: {}".format(epoch, avg_train_loss, 
-                                            avg_eval_dice_by_class))
-
-    torch.save({'epoch': epoch, 
-        'loss': avg_train_loss, 
-        'eval': avg_eval_dice_by_class,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict()},
-        'checkpoints/'+ config.model_name)
-
-    avg_eval_dice = torch.sum(avg_eval_dice_by_class) / 3
-    if avg_eval_dice > best_eval:
-        torch.save({'epoch': epoch, 
-            'loss': avg_train_loss, 
-            'eval': avg_eval_dice,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict()},
-            'checkpoints/'+ 'best_' + config.model_name)
-                
-    best_eval = avg_eval_dice
-
-    if best_eval > 0.80 or epoch > 30: # TODO: better stopping criteria. Convergence threshold?
-        break
-
-print("Training complete.")
 
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import torch
+import json
 from configparser import ConfigParser
+
 def dice_score(preds, targets):
     num_vec = 2*torch.einsum('cijk, cijk ->c', \
             [preds.squeeze(), targets.squeeze()])
@@ -15,10 +17,21 @@ class MRISegConfigParser():
         config = ConfigParser()
         config.read(config_file)
 
-        self.deterministic_train = config.getboolean('train_params', 'deterministic_train')
+        self.deterministic_train = \
+                config.getboolean('train_params', 'deterministic_train')
         self.train_split = config.getfloat('train_params', 'train_split')
         self.weight_decay = config.getfloat('train_params', 'weight_decay')
+        self.max_epochs = config.getint('train_params', 'max_epochs')
         self.data_dir = config.get('data', 'data_dir')
         self.model_name = config.get('meta', 'model_name')
-        self.modes = config.get('data', 'modes')
+        self.modes = json.loads(config.get('data', 'modes'))
+
+def save_model(name, epoch, avg_train_losses, eval_dice, model, optimizer):
+        torch.save({'epoch': epoch, 
+            'losses': avg_train_losses, 
+            'eval': eval_dice,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict()},
+            'checkpoints/'+ name)
+
 
